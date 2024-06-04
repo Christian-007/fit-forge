8e55cf8eb85cd9cddcabbdbbbcf9cf9c289b4dd7
Christian Ing Sunardi <christianingsunardi@gmail.com>
2024-06-04 18:57:35 +0700

    test: add another test case for UserService.GetAll()

--- internal/api/services/user_service_test.go
+++ internal/api/services/user_service_test.go
@@ -1,7 +1,9 @@
 package services_test
 
 import (
+	"errors"
 	"testing"
+	"time"
 
 	"github.com/Christian-007/fit-forge/internal/api/domains"
 	"github.com/Christian-007/fit-forge/internal/api/dto"
@@ -27,18 +29,27 @@ var _ = Describe("User Service", func() {
 		})
 	})
 
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
 	Describe("Get All", func() {
+		mockUserModels := []domains.UserModel{{
+			Id:        1,
+			Name:      "John Doe",
+			Email:     "johndoe@gmail.com",
+			Password:  []byte{100},
+			CreatedAt: time.Date(2024, 02, 01, 1, 1, 1, 0, time.UTC),
+		}, {
+			Id:        2,
+			Name:      "Mark",
+			Email:     "mark@gmail.com",
+			Password:  []byte{100},
+			CreatedAt: time.Date(2024, 02, 01, 1, 1, 1, 0, time.UTC),
+		}}
+
 		When("there is no error", func() {
 			It("should return a list of users", func() {
-				mockUserModels := []domains.UserModel{{
-					Id:    1,
-					Name:  "John Doe",
-					Email: "johndoe@gmail.com",
-				}, {
-					Id:    2,
-					Name:  "Mark",
-					Email: "mark@gmail.com",
-				}}
 				mockUserResponse := []dto.UserResponse{{
 					Id:    1,
 					Name:  "John Doe",
@@ -54,6 +65,17 @@ var _ = Describe("User Service", func() {
 
 				Expect(result).To(Equal(mockUserResponse))
 				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+		When("there is an error", func() {
+			It("should return an empty user array with the error", func() {
+				mockEmptyUserResponse := []dto.UserResponse{}
+
+				mockUserRepository.EXPECT().GetAll().Return(mockUserModels, errors.New("Some error"))
+				result, err := userService.GetAll()
+
+				Expect(result).To(Equal(mockEmptyUserResponse))
+				Expect(err).To(MatchError("Some error"))
 			})
 		})
 	})
