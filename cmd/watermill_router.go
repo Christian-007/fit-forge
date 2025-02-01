e98396b4e22b6b308e1c1c3c3193a6676450e563
Christian-007 <christianingsunardi@gmail.com>
2025-02-01 22:18:08 +0700

    refactor: move pubsub router from watermill_router.go

--- cmd/watermill_router.go
+++ cmd/watermill_router.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"log/slog"
 
-	"github.com/Christian-007/fit-forge/internal/app/email/delivery/pubsub"
-	emailservices "github.com/Christian-007/fit-forge/internal/app/email/services"
-	"github.com/Christian-007/fit-forge/internal/app/users/dto"
+	emailpubsub "github.com/Christian-007/fit-forge/internal/app/email/delivery/pubsub"
 	"github.com/Christian-007/fit-forge/internal/pkg/appcontext"
-	"github.com/Christian-007/fit-forge/internal/pkg/security"
-	"github.com/Christian-007/fit-forge/internal/pkg/topics"
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-amqp/v3/pkg/amqp"
 	"github.com/ThreeDotsLabs/watermill/message"
@@ -35,54 +30,7 @@ func NewWatermillRouter(amqpConfig amqp.Config, watermillLogger watermill.Logger
 		panic(err)
 	}
 
-	tokenService := security.NewTokenService(security.TokenServiceOptions{
-		SecretKey: appCtx.EnvVariableService.Get("AUTH_SECRET_KEY"),
-	})
-	emailService := emailservices.NewEmailService(emailservices.EmailServiceOptions{
-		Host:         "http://localhost:4000",
-		Cache:        appCtx.RedisClient,
-		TokenService: tokenService,
-	})
-	mailtrapSender := emailservices.NewMailtrapEmailService(emailservices.MailtrapSenderOptions{
-		Host:   appCtx.EnvVariableService.Get("EMAIL_HOST"),
-		ApiKey: appCtx.EnvVariableService.Get("MAILTRAP_API_KEY"),
-	})
-
-	emailPubSubHandler := pubsub.NewEmailPubSubHandler(pubsub.EmailPubSubHandlerOptions{
-		EmailService:   emailService,
-		MailtrapSender: mailtrapSender,
-	})
-
-	router.AddNoPublisherHandler(
-		"send_email_verification",
-		topics.UserRegistered,
-		subscriber,
-		func(msg *message.Message) error {
-			var payload dto.UserResponse
-			err := json.Unmarshal(msg.Payload, &payload)
-			if err != nil {
-				return err
-			}
-
-			appCtx.Logger.Info("Subscribing to UserRegistered",
-				slog.String("UUID", msg.UUID),
-				slog.Any("message", payload),
-			)
-
-			err = emailPubSubHandler.SendEmailVerification(payload)
-			if err != nil {
-				appCtx.Logger.Error("[send_email_verification Subscriber] Error sending an email verification",
-					slog.String("error", err.Error()),
-				)
-				return err
-			}
-
-			appCtx.Logger.Info("Successfully send an email verification",
-				slog.String("email", payload.Email),
-			)
-			return nil
-		},
-	)
+	emailpubsub.Routes(router, subscriber, appCtx)
 
 	return router
 
---
+++ internal/app/email/delivery/pubsub/router.go
@@ -0,0 +1,68 @@
+package pubsub
+
+import (
+	"encoding/json"
+	"log/slog"
+
+	emailservices "github.com/Christian-007/fit-forge/internal/app/email/services"
+	"github.com/Christian-007/fit-forge/internal/app/users/dto"
+	"github.com/Christian-007/fit-forge/internal/pkg/appcontext"
+	"github.com/Christian-007/fit-forge/internal/pkg/security"
+	"github.com/Christian-007/fit-forge/internal/pkg/topics"
+	"github.com/ThreeDotsLabs/watermill-amqp/v3/pkg/amqp"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+func Routes(router *message.Router, subscriber *amqp.Subscriber, appCtx appcontext.AppContext) {
+	// Instantiate dependencies
+	tokenService := security.NewTokenService(security.TokenServiceOptions{
+		SecretKey: appCtx.EnvVariableService.Get("AUTH_SECRET_KEY"),
+	})
+	emailService := emailservices.NewEmailService(emailservices.EmailServiceOptions{
+		Host:         "http://localhost:4000",
+		Cache:        appCtx.RedisClient,
+		TokenService: tokenService,
+	})
+	mailtrapSender := emailservices.NewMailtrapEmailService(emailservices.MailtrapSenderOptions{
+		Host:   appCtx.EnvVariableService.Get("EMAIL_HOST"),
+		ApiKey: appCtx.EnvVariableService.Get("MAILTRAP_API_KEY"),
+	})
+
+	// Instantiate handler
+	emailPubSubHandler := NewEmailPubSubHandler(EmailPubSubHandlerOptions{
+		EmailService:   emailService,
+		MailtrapSender: mailtrapSender,
+	})
+
+	// Add handler into router
+	router.AddNoPublisherHandler(
+		"send_email_verification",
+		topics.UserRegistered,
+		subscriber,
+		func(msg *message.Message) error {
+			var payload dto.UserResponse
+			err := json.Unmarshal(msg.Payload, &payload)
+			if err != nil {
+				return err
+			}
+
+			appCtx.Logger.Info("Subscribing to UserRegistered",
+				slog.String("UUID", msg.UUID),
+				slog.Any("message", payload),
+			)
+
+			err = emailPubSubHandler.SendEmailVerification(payload)
+			if err != nil {
+				appCtx.Logger.Error("[send_email_verification Subscriber] Error sending an email verification",
+					slog.String("error", err.Error()),
+				)
+				return err
+			}
+
+			appCtx.Logger.Info("Successfully send an email verification",
+				slog.String("email", payload.Email),
+			)
+			return nil
+		},
+	)
+}
